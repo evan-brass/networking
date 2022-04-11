d9c308fa225de0727847732fb8b219df45a3c653
Evan Brass <evan-brass@pm.me>
2022-04-11 11:30:08 -0700

    Spaces -> Tabs

--- client/js/graphing.mjs
+++ client/js/graphing.mjs
@@ -12,62 +12,62 @@ canvas.height = canvas_size;
 canvas.style = "max-width: vmin;";
 const node_radius = 10;
 function position_pid(pid) {
-    // We want to turn our id range [0, 2**256) into [0, 2π)
-    // We'll do that in two steps by first the bigints into [0, 5000) and then converting to number
-    const rad = Number(pid.kad_id * 5000n / (2n ** 256n)) * (2 * Math.PI / 5000);
-    const x = 1000 + Math.cos(rad) * 900;
-    const y = 1000 + -Math.sin(rad) * 900;
-    return {x, y};
+	// We want to turn our id range [0, 2**256) into [0, 2π)
+	// We'll do that in two steps by first the bigints into [0, 5000) and then converting to number
+	const rad = Number(pid.kad_id * 5000n / (2n ** 256n)) * (2 * Math.PI / 5000);
+	const x = 1000 + Math.cos(rad) * 900;
+	const y = 1000 + -Math.sin(rad) * 900;
+	return {x, y};
 }
 function draw_network() {
 
-    ctx.clearRect(0, 0, canvas_size, canvas_size);
+	ctx.clearRect(0, 0, canvas_size, canvas_size);
 
-    // Draw the radius circle:
-    ctx.beginPath();
-    ctx.arc(1000, 1000, 900, 0, 2 * Math.PI);
-    ctx.closePath();
-    ctx.stroke();
+	// Draw the radius circle:
+	ctx.beginPath();
+	ctx.arc(1000, 1000, 900, 0, 2 * Math.PI);
+	ctx.closePath();
+	ctx.stroke();
 
-    // Draw out all of the peer_ids that we've ever seen
-    for (const wr of PeerId.peer_ids.values()) {
-        const pid = wr.deref();
-        if (pid == undefined) continue;
-        const connection = PeerConnection.connections.get(pid);
-        const is_sibling = routing_table.is_sibling(pid.kad_id);
-        const bi = bucket_index(pid.kad_id);
-        if (pid == our_peerid) {
-            ctx.fillStyle = 'red';
-        } else if (is_sibling) {
-            ctx.fillStyle = 'orange';
-        } else if (connection) {
-            ctx.fillStyle = 'blue';
-        } else {
-            ctx.fillStyle = 'black';
-        }
-        const {x, y} = position_pid(pid);
-        ctx.beginPath();
-        ctx.arc(x, y, node_radius, 0, 2 * Math.PI);
-        ctx.closePath();
-        ctx.fill();
-        ctx.font = "30px bold sans-serif";
-        ctx.fillText(bi, x + 20, y + 10);
-    }
+	// Draw out all of the peer_ids that we've ever seen
+	for (const wr of PeerId.peer_ids.values()) {
+		const pid = wr.deref();
+		if (pid == undefined) continue;
+		const connection = PeerConnection.connections.get(pid);
+		const is_sibling = routing_table.is_sibling(pid.kad_id);
+		const bi = bucket_index(pid.kad_id);
+		if (pid == our_peerid) {
+			ctx.fillStyle = 'red';
+		} else if (is_sibling) {
+			ctx.fillStyle = 'orange';
+		} else if (connection) {
+			ctx.fillStyle = 'blue';
+		} else {
+			ctx.fillStyle = 'black';
+		}
+		const {x, y} = position_pid(pid);
+		ctx.beginPath();
+		ctx.arc(x, y, node_radius, 0, 2 * Math.PI);
+		ctx.closePath();
+		ctx.fill();
+		ctx.font = "30px bold sans-serif";
+		ctx.fillText(bi, x + 20, y + 10);
+	}
 
-    // Draw the sniffed connections
-    // ctx.fillStyle = 'purple';
-    // for (const [pid, conns] of sniffed_map.entries()) {
-    //     for (const conn of conns) {
-    //         // Draw an arc from pid -> conn
-    //         const {x: x1, y: y1} = position_pid(pid);
-    //         const {x: x2, y: y2} = position_pid(conn);
-    //         ctx.beginPath();
-    //         ctx.moveTo(x1, y1);
-    //         ctx.bezierCurveTo(1000, 1000, 1000, 1000, x2, y2);
-    //         ctx.stroke();
-    //     }
-    // }
+	// Draw the sniffed connections
+	// ctx.fillStyle = 'purple';
+	// for (const [pid, conns] of sniffed_map.entries()) {
+	// 	for (const conn of conns) {
+	// 		// Draw an arc from pid -> conn
+	// 		const {x: x1, y: y1} = position_pid(pid);
+	// 		const {x: x2, y: y2} = position_pid(conn);
+	// 		ctx.beginPath();
+	// 		ctx.moveTo(x1, y1);
+	// 		ctx.bezierCurveTo(1000, 1000, 1000, 1000, x2, y2);
+	// 		ctx.stroke();
+	// 	}
+	// }
 
-    requestAnimationFrame(draw_network);
+	requestAnimationFrame(draw_network);
 }
 draw_network();
--- client/js/hyperspace-client.mjs
+++ client/js/hyperspace-client.mjs
@@ -1,34 +1,34 @@
 function create_nonce() {
-    return Array.from(crypto.getRandomValues(new Uint8Array(4))).map(v => v.toString(16).padStart(2, '0')).join('');
+	return Array.from(crypto.getRandomValues(new Uint8Array(4))).map(v => v.toString(16).padStart(2, '0')).join('');
 }
 
 // Base class for outstanding requests:
 // I'm thinking that requests will be stateful but responses will not be stateful.
 class HCRequest extends EventTarget {
-    nonce = create_nonce();
-    timeout;
-    constructor({ timeout = 5000 } = {}) {
-        this.timeout = setTimeout(() => this.dispatchEvent(new CustomEvent('timedout')), timeout);
-    }
-    async handle_message(msg) {
-        // Reset the timeout
-        clearTimeout(this.timeout);
+	nonce = create_nonce();
+	timeout;
+	constructor({ timeout = 5000 } = {}) {
+		this.timeout = setTimeout(() => this.dispatchEvent(new CustomEvent('timedout')), timeout);
+	}
+	async handle_message(msg) {
+		// Reset the timeout
+		clearTimeout(this.timeout);
 
-    }
-    complete(result) {
-        clearTimeout(this.timeout);
-        this.dispatchEvent(new CustomEvent('complete', { detail: result }));
-    }
+	}
+	complete(result) {
+		clearTimeout(this.timeout);
+		this.dispatchEvent(new CustomEvent('complete', { detail: result }));
+	}
 
 }
 
 class HyperspaceClient {
-    // nonce -> HCRequest
-    outstanding_requests = new Map();
-    async handle_message({ data }) {
+	// nonce -> HCRequest
+	outstanding_requests = new Map();
+	async handle_message({ data }) {
 
-    }
-    
+	}
+	
 }
 
 export default new HyperspaceClient();
