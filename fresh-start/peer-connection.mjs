1a451ce2b53fe7e35bf12a537d2ad27f1a587574
Evan Brass <brassevan@gmail.com>
2022-11-22 08:45:53 -0800

    _

--- fresh-start/peer-connection.mjs
+++
@@ -1,6 +0,0 @@
-
-
-
-export class PeerConnection extends RTCPeerConnection {
-
-}
--- restart/entry.mjs
+++
@@ -1,5 +0,0 @@
-/**
- * Entry point for the network client.
- * 1. Bootstrap connections to the network
- * 2. Manages heartbeat (announcing ourself to the network, etc.)
- */
